66bb06571e24e5e5a8ebc779f92759ae30b2e4a3
anmolsharmaCS <52638613+anmolsharmaCS@users.noreply.github.com>
2022-11-20 12:44:08 -0500

    Add files via upload

--- Scanning/Scanning/Form1.Designer.vb
+++ Scanning/Scanning/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,315 +20,45 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.SubMenu4 = New Scanning.SubMenu()
-        Me.Button30 = New System.Windows.Forms.Button()
-        Me.Button29 = New System.Windows.Forms.Button()
-        Me.Button28 = New System.Windows.Forms.Button()
-        Me.Button27 = New System.Windows.Forms.Button()
-        Me.Button26 = New System.Windows.Forms.Button()
-        Me.Button25 = New System.Windows.Forms.Button()
-        Me.Button24 = New System.Windows.Forms.Button()
-        Me.SubMenu2 = New Scanning.SubMenu()
-        Me.Button16 = New System.Windows.Forms.Button()
-        Me.Button14 = New System.Windows.Forms.Button()
-        Me.Button13 = New System.Windows.Forms.Button()
-        Me.Button15 = New System.Windows.Forms.Button()
-        Me.Button12 = New System.Windows.Forms.Button()
-        Me.Button11 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TopMenu1 = New Scanning.TopMenu()
         Me.SubMenu1 = New Scanning.SubMenu()
-        Me.Button7 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.TopMenu1 = New Scanning.TopMenu()
-        Me.SubMenu4.SuspendLayout()
-        Me.SubMenu2.SuspendLayout()
         Me.SubMenu1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'SubMenu4
-        '
-        Me.SubMenu4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.SubMenu4.Controls.Add(Me.Button30)
-        Me.SubMenu4.Controls.Add(Me.Button29)
-        Me.SubMenu4.Controls.Add(Me.Button28)
-        Me.SubMenu4.Controls.Add(Me.Button27)
-        Me.SubMenu4.Controls.Add(Me.Button26)
-        Me.SubMenu4.Controls.Add(Me.Button25)
-        Me.SubMenu4.Controls.Add(Me.Button24)
-        Me.SubMenu4.Location = New System.Drawing.Point(361, 80)
-        Me.SubMenu4.Name = "SubMenu4"
-        Me.SubMenu4.Size = New System.Drawing.Size(233, 206)
-        Me.SubMenu4.TabIndex = 7
-        Me.SubMenu4.Visible = False
-        '
-        'Button30
-        '
-        'Me.Button30.Image = Global.Scanning.My.Resources.Resources.forward__1_
-        Me.Button30.Location = New System.Drawing.Point(184, 136)
-        Me.Button30.Name = "Button30"
-        Me.Button30.Size = New System.Drawing.Size(47, 56)
-        Me.Button30.TabIndex = 6
-        Me.Button30.UseVisualStyleBackColor = True
-        Me.Button30.Visible = False
-        '
-        'Button29
-        '
-        'Me.Button29.Image = Global.Scanning.My.Resources.Resources.resume__1_
-        Me.Button29.Location = New System.Drawing.Point(125, 136)
-        Me.Button29.Name = "Button29"
-        Me.Button29.Size = New System.Drawing.Size(47, 56)
-        Me.Button29.TabIndex = 5
-        Me.Button29.UseVisualStyleBackColor = True
-        Me.Button29.Visible = False
-        '
-        'Button28
-        '
-        'Me.Button28.Image = Global.Scanning.My.Resources.Resources.pause__1_
-        Me.Button28.Location = New System.Drawing.Point(66, 136)
-        Me.Button28.Name = "Button28"
-        Me.Button28.Size = New System.Drawing.Size(47, 56)
-        Me.Button28.TabIndex = 4
-        Me.Button28.UseVisualStyleBackColor = True
-        Me.Button28.Visible = False
-        '
-        'Button27
-        '
-        'Me.Button27.Image = Global.Scanning.My.Resources.Resources.Rewind__1_
-        Me.Button27.Location = New System.Drawing.Point(7, 136)
-        Me.Button27.Name = "Button27"
-        Me.Button27.Size = New System.Drawing.Size(47, 56)
-        Me.Button27.TabIndex = 3
-        Me.Button27.UseVisualStyleBackColor = True
-        Me.Button27.Visible = False
-        '
-        'Button26
-        '
-        Me.Button26.Enabled = False
-        'Me.Button26.Image = Global.Scanning.My.Resources.Resources.Apple_Voice_Memos_Logo_1024x575__1_
-        Me.Button26.Location = New System.Drawing.Point(62, 74)
-        Me.Button26.Name = "Button26"
-        Me.Button26.Size = New System.Drawing.Size(108, 56)
-        Me.Button26.TabIndex = 2
-        Me.Button26.UseVisualStyleBackColor = True
-        Me.Button26.Visible = False
-        '
-        'Button25
-        '
-        Me.Button25.Enabled = False
-        'Me.Button25.Image = Global.Scanning.My.Resources.Resources.apple_podcast__1_
-        Me.Button25.Location = New System.Drawing.Point(121, 12)
-        Me.Button25.Name = "Button25"
-        Me.Button25.Size = New System.Drawing.Size(108, 56)
-        Me.Button25.TabIndex = 1
-        Me.Button25.UseVisualStyleBackColor = True
-        Me.Button25.Visible = False
-        '
-        'Button24
-        '
-        Me.Button24.Enabled = False
-        'Me.Button24.Image = Global.Scanning.My.Resources.Resources.radio__1_
-        Me.Button24.Location = New System.Drawing.Point(7, 12)
-        Me.Button24.Name = "Button24"
-        Me.Button24.Size = New System.Drawing.Size(108, 56)
-        Me.Button24.TabIndex = 0
-        Me.Button24.UseVisualStyleBackColor = True
-        Me.Button24.Visible = False
-        '
-        'SubMenu2
-        '
-        Me.SubMenu2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.SubMenu2.Controls.Add(Me.Button16)
-        Me.SubMenu2.Controls.Add(Me.Button14)
-        Me.SubMenu2.Controls.Add(Me.Button13)
-        Me.SubMenu2.Controls.Add(Me.Button15)
-        Me.SubMenu2.Controls.Add(Me.Button12)
-        Me.SubMenu2.Controls.Add(Me.Button11)
-        Me.SubMenu2.Controls.Add(Me.Button10)
-        Me.SubMenu2.Controls.Add(Me.Button9)
-        Me.SubMenu2.Controls.Add(Me.Button8)
-        Me.SubMenu2.Enabled = False
-        Me.SubMenu2.Location = New System.Drawing.Point(120, 80)
-        Me.SubMenu2.Name = "SubMenu2"
-        Me.SubMenu2.Size = New System.Drawing.Size(240, 258)
-        Me.SubMenu2.TabIndex = 6
-        Me.SubMenu2.Visible = False
-        '
-        'Button16
-        '
-        'Me.Button16.Image = Global.Scanning.My.Resources.Resources.forward__1_
-        Me.Button16.Location = New System.Drawing.Point(166, 193)
-        Me.Button16.Name = "Button16"
-        Me.Button16.Size = New System.Drawing.Size(47, 56)
-        Me.Button16.TabIndex = 8
-        Me.Button16.UseVisualStyleBackColor = True
-        Me.Button16.Visible = False
-        '
-        'Button14
-        '
-        'Me.Button14.Image = Global.Scanning.My.Resources.Resources.pause__1_
-        Me.Button14.Location = New System.Drawing.Point(67, 193)
-        Me.Button14.Name = "Button14"
-        Me.Button14.Size = New System.Drawing.Size(47, 56)
-        Me.Button14.TabIndex = 6
-        Me.Button14.UseVisualStyleBackColor = True
-        Me.Button14.Visible = False
-        '
-        'Button13
-        '
-        'Me.Button13.Image = Global.Scanning.My.Resources.Resources.resume__1_
-        Me.Button13.Location = New System.Drawing.Point(117, 193)
-        Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(47, 56)
-        Me.Button13.TabIndex = 7
-        Me.Button13.UseVisualStyleBackColor = True
-        Me.Button13.Visible = False
-        '
-        'Button15
-        '
-        'Me.Button15.Image = Global.Scanning.My.Resources.Resources.Rewind__1_
-        Me.Button15.Location = New System.Drawing.Point(17, 193)
-        Me.Button15.Name = "Button15"
-        Me.Button15.Size = New System.Drawing.Size(47, 56)
-        Me.Button15.TabIndex = 5
-        Me.Button15.UseVisualStyleBackColor = True
-        Me.Button15.Visible = False
-        '
-        'Button12
-        '
-        'Me.Button12.Image = Global.Scanning.My.Resources.Resources.playOnce__1_
-        Me.Button12.Location = New System.Drawing.Point(88, 135)
-        Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(56, 52)
-        Me.Button12.TabIndex = 3
-        Me.Button12.UseVisualStyleBackColor = True
-        '
-        'Button11
-        '
-        Me.Button11.Enabled = False
-        'Me.Button11.Image = Global.Scanning.My.Resources.Resources.repeat__1_
-        Me.Button11.Location = New System.Drawing.Point(159, 135)
-        Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(56, 52)
-        Me.Button11.TabIndex = 4
-        Me.Button11.UseVisualStyleBackColor = True
-        '
-        'Button10
-        '
-        Me.Button10.Enabled = False
-        'Me.Button10.Image = Global.Scanning.My.Resources.Resources.shuffb__1_
-        Me.Button10.Location = New System.Drawing.Point(17, 135)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(56, 52)
-        Me.Button10.TabIndex = 2
-        Me.Button10.UseVisualStyleBackColor = True
-        '
-        'Button9
-        '
-        Me.Button9.Enabled = False
-        'Me.Button9.Image = Global.Scanning.My.Resources.Resources.wideA__1_
-        Me.Button9.Location = New System.Drawing.Point(17, 72)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(198, 44)
-        Me.Button9.TabIndex = 1
-        Me.Button9.UseVisualStyleBackColor = True
-        '
-        'Button8
-        '
-        Me.Button8.Enabled = False
-        'Me.Button8.Image = Global.Scanning.My.Resources.Resources.wideP__1_
-        Me.Button8.Location = New System.Drawing.Point(17, 22)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(198, 44)
-        Me.Button8.TabIndex = 0
-        Me.Button8.UseVisualStyleBackColor = True
-        '
-        'SubMenu1
-        '
-        Me.SubMenu1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.SubMenu1.Controls.Add(Me.Button7)
-        Me.SubMenu1.Controls.Add(Me.Button6)
-        Me.SubMenu1.Controls.Add(Me.Button5)
-        Me.SubMenu1.Controls.Add(Me.Button4)
-        Me.SubMenu1.Controls.Add(Me.Button1)
-        Me.SubMenu1.Controls.Add(Me.Button3)
-        Me.SubMenu1.Controls.Add(Me.Button2)
-        Me.SubMenu1.Location = New System.Drawing.Point(12, 3)
-        Me.SubMenu1.Name = "SubMenu1"
-        Me.SubMenu1.Size = New System.Drawing.Size(107, 494)
-        Me.SubMenu1.TabIndex = 4
-        '
-        'Button7
-        '
-        Me.Button7.Image = CType(resources.GetObject("Button7.Image"), System.Drawing.Image)
-        Me.Button7.Location = New System.Drawing.Point(7, 143)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(82, 64)
-        Me.Button7.TabIndex = 2
-        Me.Button7.UseVisualStyleBackColor = True
-        '
-        'Button6
-        '
-        Me.Button6.Image = CType(resources.GetObject("Button6.Image"), System.Drawing.Image)
-        Me.Button6.Location = New System.Drawing.Point(7, 202)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(82, 64)
-        Me.Button6.TabIndex = 3
-        Me.Button6.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.Image = CType(resources.GetObject("Button5.Image"), System.Drawing.Image)
-        Me.Button5.Location = New System.Drawing.Point(7, 415)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(82, 64)
-        Me.Button5.TabIndex = 6
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
-        Me.Button4.Location = New System.Drawing.Point(7, 347)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(82, 64)
-        Me.Button4.TabIndex = 5
-        Me.Button4.UseVisualStyleBackColor = True
         '
         'Button1
         '
         Me.Button1.Image = Global.Scanning.My.Resources.Resources.help
-        Me.Button1.Location = New System.Drawing.Point(7, 7)
+        Me.Button1.Location = New System.Drawing.Point(7, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(82, 64)
+        Me.Button1.Size = New System.Drawing.Size(97, 75)
         Me.Button1.TabIndex = 0
         Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Image = Global.Scanning.My.Resources.Resources.movie__1_
-        Me.Button3.Location = New System.Drawing.Point(7, 279)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(82, 64)
-        Me.Button3.TabIndex = 4
-        Me.Button3.UseVisualStyleBackColor = True
         '
         'Button2
         '
         Me.Button2.Image = Global.Scanning.My.Resources.Resources.musicnote
-        Me.Button2.Location = New System.Drawing.Point(7, 75)
+        Me.Button2.Location = New System.Drawing.Point(7, 165)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(82, 64)
+        Me.Button2.Size = New System.Drawing.Size(97, 75)
         Me.Button2.TabIndex = 1
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Image = Global.Scanning.My.Resources.Resources.movie__1_
+        Me.Button3.Location = New System.Drawing.Point(7, 246)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(97, 75)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'TopMenu1
         '
@@ -338,20 +68,57 @@ Partial Class Form1
         Me.TopMenu1.Size = New System.Drawing.Size(36, 36)
         Me.TopMenu1.TabIndex = 3
         '
+        'SubMenu1
+        '
+        Me.SubMenu1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SubMenu1.Controls.Add(Me.Button6)
+        Me.SubMenu1.Controls.Add(Me.Button5)
+        Me.SubMenu1.Controls.Add(Me.Button4)
+        Me.SubMenu1.Controls.Add(Me.Button1)
+        Me.SubMenu1.Controls.Add(Me.Button3)
+        Me.SubMenu1.Controls.Add(Me.Button2)
+        Me.SubMenu1.Location = New System.Drawing.Point(43, 39)
+        Me.SubMenu1.Name = "SubMenu1"
+        Me.SubMenu1.Size = New System.Drawing.Size(228, 416)
+        Me.SubMenu1.TabIndex = 4
+        '
+        'Button6
+        '
+        Me.Button6.Image = Global.Scanning.My.Resources.Resources.volumecontrol
+        Me.Button6.Location = New System.Drawing.Point(110, 165)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(97, 75)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Image = Global.Scanning.My.Resources.Resources.home
+        Me.Button5.Location = New System.Drawing.Point(7, 327)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(97, 75)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Image = Global.Scanning.My.Resources.Resources.television
+        Me.Button4.Location = New System.Drawing.Point(7, 84)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(97, 75)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Scanning.My.Resources.Resources.background2
         Me.ClientSize = New System.Drawing.Size(596, 498)
-        Me.Controls.Add(Me.SubMenu4)
-        Me.Controls.Add(Me.SubMenu2)
         Me.Controls.Add(Me.SubMenu1)
         Me.Controls.Add(Me.TopMenu1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.SubMenu4.ResumeLayout(False)
-        Me.SubMenu2.ResumeLayout(False)
         Me.SubMenu1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -365,23 +132,4 @@ Partial Class Form1
     Friend WithEvents Button4 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents Button5 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents SubMenu2 As SubMenu
-    Friend WithEvents Button9 As Button
-    Friend WithEvents Button8 As Button
-    Friend WithEvents Button10 As Button
-    Friend WithEvents Button12 As Button
-    Friend WithEvents Button11 As Button
-    Friend WithEvents Button16 As Button
-    Friend WithEvents Button14 As Button
-    Friend WithEvents Button13 As Button
-    Friend WithEvents Button15 As Button
-    Friend WithEvents SubMenu4 As SubMenu
-    Friend WithEvents Button30 As Button
-    Friend WithEvents Button29 As Button
-    Friend WithEvents Button28 As Button
-    Friend WithEvents Button27 As Button
-    Friend WithEvents Button26 As Button
-    Friend WithEvents Button25 As Button
-    Friend WithEvents Button24 As Button
 End Class
