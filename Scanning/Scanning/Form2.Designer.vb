517db0f700d1247e4b126233eaf3b6e1bf90cd9d
anmolsharmaCS <52638613+anmolsharmaCS@users.noreply.github.com>
2022-11-20 17:25:01 -0500

    Add files via upload

--- Scanning/Scanning/Form2.Designer.vb
+++ Scanning/Scanning/Form2.Designer.vb
@@ -22,16 +22,7 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TopMenu1 = New Scanning.TopMenu()
-        Me.SubMenu1 = New Scanning.SubMenu()
-        Me.Button11 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.SubMenu2 = New Scanning.SubMenu()
-        Me.Button12 = New System.Windows.Forms.Button()
-        Me.Button13 = New System.Windows.Forms.Button()
-        Me.Button14 = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
@@ -40,113 +31,44 @@ Partial Class Form2
         Me.Button7 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
         Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.SubMenu3 = New Scanning.SubMenu()
+        Me.Button25 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.SubMenu1 = New Scanning.SubMenu()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.SubMenu2 = New Scanning.SubMenu()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.TopMenu1 = New Scanning.TopMenu()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SubMenu3.SuspendLayout()
         Me.SubMenu1.SuspendLayout()
         Me.SubMenu2.SuspendLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'TopMenu1
-        '
-        Me.TopMenu1.Location = New System.Drawing.Point(752, 13)
-        Me.TopMenu1.Name = "TopMenu1"
-        Me.TopMenu1.ScanningInterval = 1000
-        Me.TopMenu1.Size = New System.Drawing.Size(36, 36)
-        Me.TopMenu1.TabIndex = 0
-        '
-        'SubMenu1
-        '
-        Me.SubMenu1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.SubMenu1.Controls.Add(Me.Button15)
-        Me.SubMenu1.Controls.Add(Me.Button11)
-        Me.SubMenu1.Controls.Add(Me.Button10)
-        Me.SubMenu1.Controls.Add(Me.Button9)
-        Me.SubMenu1.Controls.Add(Me.Button8)
-        Me.SubMenu1.Location = New System.Drawing.Point(227, 102)
-        Me.SubMenu1.Name = "SubMenu1"
-        Me.SubMenu1.Size = New System.Drawing.Size(357, 190)
-        Me.SubMenu1.TabIndex = 1
-        '
-        'Button11
-        '
-        Me.Button11.Image = Global.Scanning.My.Resources.Resources.cancel
-        Me.Button11.Location = New System.Drawing.Point(3, 95)
-        Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(96, 86)
-        Me.Button11.TabIndex = 3
-        Me.Button11.UseVisualStyleBackColor = True
-        '
-        'Button10
-        '
-        Me.Button10.Image = Global.Scanning.My.Resources.Resources.CallAgain__1_
-        Me.Button10.Location = New System.Drawing.Point(231, 3)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(96, 86)
-        Me.Button10.TabIndex = 2
-        Me.Button10.UseVisualStyleBackColor = True
-        '
-        'Button9
-        '
-        Me.Button9.Image = Global.Scanning.My.Resources.Resources.notUrgent__1_
-        Me.Button9.Location = New System.Drawing.Point(119, 3)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(96, 86)
-        Me.Button9.TabIndex = 1
-        Me.Button9.UseVisualStyleBackColor = True
-        '
-        'Button8
-        '
-        Me.Button8.Image = Global.Scanning.My.Resources.Resources.urgent__1_
-        Me.Button8.Location = New System.Drawing.Point(3, 3)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(96, 86)
-        Me.Button8.TabIndex = 0
-        Me.Button8.UseVisualStyleBackColor = True
-        '
-        'SubMenu2
-        '
-        Me.SubMenu2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.SubMenu2.Controls.Add(Me.Button12)
-        Me.SubMenu2.Controls.Add(Me.Button13)
-        Me.SubMenu2.Controls.Add(Me.Button14)
-        Me.SubMenu2.Location = New System.Drawing.Point(227, 298)
-        Me.SubMenu2.Name = "SubMenu2"
-        Me.SubMenu2.Size = New System.Drawing.Size(357, 101)
-        Me.SubMenu2.TabIndex = 3
-        '
-        'Button12
-        '
-        Me.Button12.Image = Global.Scanning.My.Resources.Resources.transfer__1_
-        Me.Button12.Location = New System.Drawing.Point(3, 4)
-        Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(96, 86)
-        Me.Button12.TabIndex = 0
-        Me.Button12.UseVisualStyleBackColor = True
-        '
-        'Button13
-        '
-        Me.Button13.Image = Global.Scanning.My.Resources.Resources.bed__1_
-        Me.Button13.Location = New System.Drawing.Point(119, 4)
-        Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(96, 86)
-        Me.Button13.TabIndex = 1
-        Me.Button13.UseVisualStyleBackColor = True
-        '
-        'Button14
-        '
-        Me.Button14.Image = Global.Scanning.My.Resources.Resources.toilet__1_
-        Me.Button14.Location = New System.Drawing.Point(231, 4)
-        Me.Button14.Name = "Button14"
-        Me.Button14.Size = New System.Drawing.Size(96, 86)
-        Me.Button14.TabIndex = 2
-        Me.Button14.UseVisualStyleBackColor = True
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.Scanning.My.Resources.Resources.assisstant
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 99)
+        Me.PictureBox1.Location = New System.Drawing.Point(15, 12)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(209, 300)
+        Me.PictureBox1.Size = New System.Drawing.Size(214, 300)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
@@ -168,7 +90,6 @@ Partial Class Form2
         Me.Button1.Size = New System.Drawing.Size(96, 86)
         Me.Button1.TabIndex = 0
         Me.Button1.UseVisualStyleBackColor = True
-        Me.Button1.Visible = False
         '
         'Button3
         '
@@ -216,31 +137,267 @@ Partial Class Form2
         Me.Button5.TabIndex = 4
         Me.Button5.UseVisualStyleBackColor = False
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 325)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 19)
+        Me.Label1.TabIndex = 4
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.Scanning.My.Resources.Resources.nurseWalk
+        Me.PictureBox2.Location = New System.Drawing.Point(18, 353)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(83, 93)
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Button11
+        '
+        Me.Button11.Image = Global.Scanning.My.Resources.Resources.cancel
+        Me.Button11.Location = New System.Drawing.Point(3, 14)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(90, 67)
+        Me.Button11.TabIndex = 0
+        Me.Button11.Text = "Cancel"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Image = Global.Scanning.My.Resources.Resources.CallAgain__1_
+        Me.Button10.Location = New System.Drawing.Point(3, 87)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(90, 67)
+        Me.Button10.TabIndex = 1
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Image = Global.Scanning.My.Resources.Resources.notUrgent__1_
+        Me.Button9.Location = New System.Drawing.Point(136, 3)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(96, 86)
+        Me.Button9.TabIndex = 1
+        Me.Button9.Text = "Not Urgent"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
         'Button15
         '
         Me.Button15.Image = Global.Scanning.My.Resources.Resources.home
-        Me.Button15.Location = New System.Drawing.Point(119, 95)
+        Me.Button15.Location = New System.Drawing.Point(268, 13)
         Me.Button15.Name = "Button15"
-        Me.Button15.Size = New System.Drawing.Size(96, 86)
-        Me.Button15.TabIndex = 4
+        Me.Button15.Size = New System.Drawing.Size(80, 67)
+        Me.Button15.TabIndex = 2
         Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Image = Global.Scanning.My.Resources.Resources.urgent__1_
+        Me.Button8.Location = New System.Drawing.Point(3, 3)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(96, 86)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = "Urgent"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Image = Global.Scanning.My.Resources.Resources.transfer__1_
+        Me.Button12.Location = New System.Drawing.Point(8, 77)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(80, 67)
+        Me.Button12.TabIndex = 1
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Image = Global.Scanning.My.Resources.Resources.bed__1_
+        Me.Button13.Location = New System.Drawing.Point(8, 4)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(80, 67)
+        Me.Button13.TabIndex = 0
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Image = Global.Scanning.My.Resources.Resources.toilet__1_
+        Me.Button14.Location = New System.Drawing.Point(8, 150)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(80, 67)
+        Me.Button14.TabIndex = 2
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'SubMenu3
+        '
+        Me.SubMenu3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SubMenu3.Controls.Add(Me.Button25)
+        Me.SubMenu3.Controls.Add(Me.Button23)
+        Me.SubMenu3.Controls.Add(Me.Button22)
+        Me.SubMenu3.Location = New System.Drawing.Point(506, 109)
+        Me.SubMenu3.Name = "SubMenu3"
+        Me.SubMenu3.Size = New System.Drawing.Size(96, 238)
+        Me.SubMenu3.TabIndex = 6
+        '
+        'Button25
+        '
+        Me.Button25.Image = Global.Scanning.My.Resources.Resources._return
+        Me.Button25.Location = New System.Drawing.Point(10, 164)
+        Me.Button25.Name = "Button25"
+        Me.Button25.Size = New System.Drawing.Size(75, 68)
+        Me.Button25.TabIndex = 2
+        Me.Button25.Text = "return"
+        Me.Button25.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Image = Global.Scanning.My.Resources.Resources.CallAgain__1_
+        Me.Button23.Location = New System.Drawing.Point(10, 90)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(75, 68)
+        Me.Button23.TabIndex = 1
+        Me.Button23.Text = "Call again"
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Image = Global.Scanning.My.Resources.Resources.cancel
+        Me.Button22.Location = New System.Drawing.Point(10, 12)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(75, 68)
+        Me.Button22.TabIndex = 0
+        Me.Button22.Text = "Cancel"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'SubMenu1
+        '
+        Me.SubMenu1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SubMenu1.Controls.Add(Me.Button18)
+        Me.SubMenu1.Controls.Add(Me.Button16)
+        Me.SubMenu1.Controls.Add(Me.Button17)
+        Me.SubMenu1.Location = New System.Drawing.Point(235, 6)
+        Me.SubMenu1.Name = "SubMenu1"
+        Me.SubMenu1.Size = New System.Drawing.Size(366, 97)
+        Me.SubMenu1.TabIndex = 1
+        '
+        'Button18
+        '
+        Me.Button18.Image = Global.Scanning.My.Resources.Resources.home
+        Me.Button18.Location = New System.Drawing.Point(271, 15)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(89, 75)
+        Me.Button18.TabIndex = 2
+        Me.Button18.Text = "Home"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Image = Global.Scanning.My.Resources.Resources.urgent__1_
+        Me.Button16.Location = New System.Drawing.Point(12, 15)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(89, 65)
+        Me.Button16.TabIndex = 0
+        Me.Button16.Text = "Urgent"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Image = Global.Scanning.My.Resources.Resources.notUrgent__1_
+        Me.Button17.Location = New System.Drawing.Point(141, 15)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(89, 65)
+        Me.Button17.TabIndex = 1
+        Me.Button17.Text = "Not Urgent"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'SubMenu2
+        '
+        Me.SubMenu2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SubMenu2.Controls.Add(Me.Button24)
+        Me.SubMenu2.Controls.Add(Me.Button21)
+        Me.SubMenu2.Controls.Add(Me.Button20)
+        Me.SubMenu2.Controls.Add(Me.Button19)
+        Me.SubMenu2.Enabled = False
+        Me.SubMenu2.Location = New System.Drawing.Point(378, 109)
+        Me.SubMenu2.Name = "SubMenu2"
+        Me.SubMenu2.Size = New System.Drawing.Size(98, 324)
+        Me.SubMenu2.TabIndex = 3
+        '
+        'Button24
+        '
+        Me.Button24.Image = Global.Scanning.My.Resources.Resources._return
+        Me.Button24.Location = New System.Drawing.Point(10, 242)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(73, 68)
+        Me.Button24.TabIndex = 3
+        Me.Button24.Text = "return"
+        Me.Button24.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Image = Global.Scanning.My.Resources.Resources.bed__1_
+        Me.Button21.Location = New System.Drawing.Point(10, 164)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(75, 68)
+        Me.Button21.TabIndex = 2
+        Me.Button21.Text = "bed"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Image = Global.Scanning.My.Resources.Resources.transfer__1_
+        Me.Button20.Location = New System.Drawing.Point(10, 90)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(75, 68)
+        Me.Button20.TabIndex = 1
+        Me.Button20.Text = "moving"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Image = Global.Scanning.My.Resources.Resources.toilet__1_
+        Me.Button19.Location = New System.Drawing.Point(10, 12)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(75, 68)
+        Me.Button19.TabIndex = 0
+        Me.Button19.Text = "Button19"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'TopMenu1
+        '
+        Me.TopMenu1.Location = New System.Drawing.Point(752, 13)
+        Me.TopMenu1.Name = "TopMenu1"
+        Me.TopMenu1.ScanningInterval = 1000
+        Me.TopMenu1.Size = New System.Drawing.Size(36, 36)
+        Me.TopMenu1.TabIndex = 0
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Scanning.My.Resources.Resources.background2
-        Me.ClientSize = New System.Drawing.Size(596, 498)
+        Me.ClientSize = New System.Drawing.Size(613, 498)
+        Me.Controls.Add(Me.SubMenu3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.SubMenu1)
         Me.Controls.Add(Me.SubMenu2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.TopMenu1)
         Me.Name = "Form2"
         Me.Text = "Form2"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SubMenu3.ResumeLayout(False)
         Me.SubMenu1.ResumeLayout(False)
         Me.SubMenu2.ResumeLayout(False)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -263,4 +420,18 @@ Partial Class Form2
     Friend WithEvents Button13 As Button
     Friend WithEvents Button14 As Button
     Friend WithEvents Button15 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents SubMenu3 As SubMenu
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Button24 As Button
 End Class
--- Scanning/Scanning/Form2.vb
+++ Scanning/Scanning/Form2.vb
@@ -1,54 +1,113 @@
-﻿Imports System.ComponentModel
-
+﻿Imports System.Threading
 Public Class Form2
     Private tp As TopMenu
     'tp = TopMenu1
-
-    Private MyParentHall As FloorHallways
-    Private MyParentApartment As UserApartment
-
-    Public Sub New(parentForm)
+    Public Sub New()
 
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
-        TopMenu1.Initialize(SubMenu1, SubMenu2)
-        Dim num As Integer = Me.Controls.Count
-
-        If parentForm.GetType() Is GetType(FloorHallways) Then
-            MyParentHall = parentForm
-        ElseIf parentForm.GetType() Is GetType(UserApartment) Then
-            MyParentApartment = parentForm
-        End If
+        TopMenu1.Initialize(SubMenu1)
 
     End Sub
 
 
-    Private Sub button_click(sender As Object, e As EventArgs) Handles Button9.Click, Button8.Click, Button14.Click, Button13.Click, Button12.Click, Button11.Click, Button10.Click
-        TopMenu1.StopScanning()
-        Dim b As Button = sender
-        MsgBox(b.ToString() & " was selected")
 
-        TopMenu1.ResumeScanning()
-
-    End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles Me.Load
         TopMenu1.StartScanning()
     End Sub
 
-    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Console.WriteLine(PictureBox1.Location.X)
+        ' Console.WriteLine(PictureBox1.Location.Y)
+
+        While (PictureBox2.Location.X < 250)
+            PictureBox2.Location = New Point(PictureBox2.Location.X + 1, PictureBox2.Location.Y)
+        End While
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+
+        TopMenu1.StopScanning()
+
+
+        SubMenu2.Enabled = True
+
+        TopMenu1.Initialize(SubMenu2)
+        TopMenu1.StartScanning()
+
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click, Button20.Click, Button21.Click
+
+        Timer1.Enabled = False
+        Label1.Text = ""
+
+        Timer1.Enabled = True
+
+        Dim ThisMoment As Date
+        ThisMoment = Now
+        Label1.Text = "Your assistance should arrive at: (" & ThisMoment.Hour & "00:" & ThisMoment.Minute + 2 & ":00)"
+
+        TopMenu1.StopScanning()
+
+        TopMenu1.Initialize(SubMenu3)
+        TopMenu1.StartScanning()
+
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click, Button25.Click
+        TopMenu1.StopScanning()
+
+        TopMenu1.Initialize(SubMenu1)
+        TopMenu1.StartScanning()
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+
+        Timer1.Enabled = False
+        Label1.Text = ""
+
+        Timer1.Enabled = True
+        Dim ThisMoment As Date
+        ThisMoment = Now
+        Label1.Text = "Your assistance should arrive at: (" & ThisMoment.Hour & "00:" & ThisMoment.Minute + 2 & ":00)"
+
+
+        TopMenu1.StopScanning()
+
+        TopMenu1.Initialize(SubMenu3)
+        TopMenu1.StartScanning()
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        Timer1.Enabled = False
+        Label1.Text = ""
+        PictureBox2.Location = New Point(15, 350)
+
+        TopMenu1.StopScanning()
+
+        TopMenu1.Initialize(SubMenu1)
+        TopMenu1.StartScanning()
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        Label1.Text = ""
+        PictureBox2.Location = New Point(15, 350)
+        Timer1.Enabled = False
+        Timer1.Enabled = True
+        Dim ThisMoment As Date
+        ThisMoment = Now
+        Label1.Text = "Your assistance should arrive at: (" & ThisMoment.Hour & "00:" & ThisMoment.Minute + 2 & ":00)"
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
         Dim home As MainForm = New MainForm()
         home.Show()
         Me.Hide()
-    End Sub
-
-    Private Sub Form2_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
-        If MyParentHall IsNot Nothing Then
-            MyParentHall.ResumeScanning()
-        ElseIf MyParentApartment IsNot Nothing Then
-            MyParentApartment.ResumeScanning()
-        End If
     End Sub
 End Class
